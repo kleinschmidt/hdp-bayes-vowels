1b028b24014b0531e2dcaed03ec3259f343523b2
Dave Kleinschmidt <dfklein@umd.edu>
2009-10-29 11:20:51 -0400

    Initial implementation of posterior sampler for beta

--- rasmussen2000.r
+++ rasmussen2000.r
@@ -18,6 +18,21 @@ dBetaPosterior <- function(Beta, S, w) {
     }
 }
 
+## Sample from the conditional posterior on beta (component precision prior shape) using
+## ARS
+rBetaPosterior <- function(num=1, S, w) {
+    k <- dim(S)[3]
+    h <- function(y) {
+        -k*log(gamma(exp(y)/2)) - .5/exp(y) - .5*y - 1.5*log(2) +
+            .5*exp(y) * (k*(y+log(2)) + sum(w*S + log(w*S)))
+    }
+    hprim <- function(y) {
+        -k*exp(y)*0.5*digamma(0.5*exp(y)) + 0.5/exp(y) - 0.5 +
+            0.5*exp(y)(k*(y+log(2)+1) + sum(w*S + log(w*S)))
+    }
+    return(exp(ars(n=num, f=h, fprima=hprim)))
+}
+
 ## Sample from the conditional posterior on the category spread parameter alpha
 ## using ARS
 rAlphaPosterior <- function(num=1, N) {
