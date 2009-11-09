edc44b879a65ea38591c33f1181e75d5441e7e07
Dave Kleinschmidt <dfklein@umd.edu>
2009-11-09 10:44:15 -0500

    changed Gibbs sampler in rasmussen2000.r to a function, added data logging

--- rasmussen2000.r
+++ rasmussen2000.r
@@ -52,101 +52,112 @@ rAlphaPosterior <- function(num=1, N) {
 
 
 ## Gibbs sampler for parameter-free DP Gaussian mixture model
+dpgaussRun <- function(X, nIter) {
+    ## preallocate list for data
+    data <- vector("list", nIter)
+    
+    ## observations are in X, dim(X) = c(nObs,nDim)
+    nObs <- dim(X)[1]
+    nDim <- dim(X)[2]
+    
+    meanX <- mean(X)
+    varX <- var(X)
+    precisX <- solve(varX)
 
-## data is in X, dim(X) = c(nObs,nDim)
-meanX <- mean(X)
-varX <- var(X)
-precisionX <- solve(varX)
+    lambda <- rnorm(n=1, mean=mean(X), sd=sd(X))
+    r <- rgamma(n=1, shape=1, scale=solve(varX))
+    MU <- rnorm(n=1, mean=lambda, sd=solve(sqrt(r)))
 
-lambda <- rmnorm(n=1, mean=colMeans(X), sd=sd(X))
-r <- rgamma(n=1, shape=1, scale=solve(varX))
-MU <- rnorm(n=1, mean=lambda, sd=solve(sqrt(r)))
+    beta <- solve(rgamma(n=1, shape=1, scale=1))
+    w <- rgamma(n=1, shape=1, scale=varX)
+    S <- rgamma(n=1, shape=beta, rate=w)
 
-beta <- solve(rgamma(n=1, shape=1, scale=1))
-w <- rgamma(n=1, shape=1, scale=varX)
-S <- rgamma(n=1, shape=beta, rate=w)
+    Z <- rep(1,nObs)
+    comps <- 1
+    nComps <- 1
+    N <- nObs
+    
+    ## initialize hyperparameters
+    for (iter in 1:nIters) {
+        ## update hyperparameters
+        lambda <- rnorm(n=1,
+                        mean=(meanX*precisX + r*sum(MU))/(precisX+nComps*r),
+                        sd=solve(sqrt(precisX + nComps*r)))
+        r <- rgamma(n=1,
+                    shape=(nComps+1),
+                    rate=(varX + sum((MU-lambda)^2))/(nComps+1))
 
-Z <- rep(1,nObs)
-comps <- 1
-N <- nObs
+        w <- rgamma(n=1,
+                    shape=(nComps*beta+1),
+                    rate=(precisX + beta*sum(S))/(nComps*beta+1))
+        beta <- rBetaPosterior(num=1, S, w)
 
-## initialize hyperparameters
-for (iter in 1:nIters) {
-    ## update hyperparameters
-    lambda <- rnorm(n=1,
-                    mean=(meanX*precisX + r*sum(MU))/(precisX+k*r),
-                    sd=solve(sqrt(precisX + k*r)))
-    r <- rgamma(n=1,
-                shape=(k+1),
-                rate=(varX + sum((MU-lambda)^2))/(k+1))
+        alpha <- rAlphaPosterior(num=1, N)
 
-    w <- rgamma(n=1,
-                shape=(k*beta+1),
-                rate=(precisX + beta*sum(S))/(k*beta+1))
-    beta <- rBetaPosterior(num=1, S, w)
+        ## update component parameters
+        for (co in 1:nComps) {
+            Xco <- X[Z==comps[co]]
+            MU[co] <- rnorm(n=1,
+                            mean=(mean(Xco)*N[co]*S[co] + lambda*r) / (N[co]*S[co] + r),
+                            sd=solve(sqrt(N[co]*S[co] + r)) )
+            S[co] <- rgamma(n=1,
+                            shape=(beta+N[co]),
+                            rate=(w*beta + sum((Xco-MU[co])^2))/(beta+N[co]) )
+        }
 
-    alpha <- rAlphaPosterior(num=1, N)
+        ## update latent variables (category/component assignments)
 
-    ## update component parameters
-    for (co in 1:k) {
-        Xco <- X[Z==comps[co]]
-        MU[co] <- rnorm(n=1,
-                        mean=(mean(Xco)*N[co]*S[co] + lambda*r) / (N[co]*S[co] + r),
-                        sd=solve(sqrt(N[co]*S[co] + r)) )
-        S[co] <- rgamma(n=1,
-                        shape=(beta+N[co]),
-                        rate=(w*beta + sum((Xco-MU[co])^2))/(beta+N[co]) )
+        for (j in 1:nObs) {
+            probs <- rep(0,nComps+1)
+            for (co in 1:nComps) {
+                probs[co] <- dnorm(X[j], mean=MU[co], sd=solve(sqrt(S[co]))) *
+                    (N[co]-(Z[j]==comps[co]))
+            }
+            ## draw component parameter values from their priors for unrepresented comps
+            mu <- rnorm(n=1,mean=lambda,sd=solve(sqrt(r)))
+            s <- rgamma(n=1,shape=beta, rate=w)
+            probs[nComps+1] <- dnorm(X[j],
+                                     mean=mu,
+                                     sd=solve(sqrt(s))) * alpha
+            newComp <- which(rmultinom(n=1, size=1, prob=probs)==1)
+            if (newComp > nComps) {
+                ## new component
+                nComps <- nComps + 1
+                ## find a new label
+                comps <- append(comps, min(setdiff(1:(max(comps)+1), comps)))
+                Znew <- tail(comps,1)
+                ## store initial component precision and mean
+                S <- c(S,s) #array(c(S, solve(sigma)), c(d,d,nComps))
+                MU <- c(MU,mu) #rbind(MU, mu)
+                ## update counts
+                N <- append(N, 1)
+                N[comps==Z[obs]] <- N[comps==Z[obs]] - 1
+            } else {
+                ## old component
+                ## just update counts:
+                Znew <- comps[newComp]
+                N[newComp] <- N[newComp] + 1
+                N[comps==Z[obs]] <- N[comps==Z[obs]] - 1
+            }
+
+            ## Delete a component if its last observation is removed
+            if (N[comps==Z[obs]] == 0) {
+                whichComp <- which(comps==Z[obs])
+                comps <- comps[-whichComp]
+                nComps <- nComps - 1
+                N <- N[-whichComp]
+                MU <- array(data=MU[-whichComp,], dim=c(nComps,d))
+                S <- array(data=S[,,-whichComp], dim=c(d,d,nComps))
+            }
+
+            ## Record the new component assignment for this observation
+            Z[obs] <- Znew
+        }
+
+        ## visualization
+        compHist(X, Z, comps, N, MU, S)
+        ## record state after this iteration
+        data[[iter]] <- list(Z=Z, comps=comps, MU=MU, S=S, N=N,
+                             alpha=alpha, beta=beta, w=w, r=r, lambda=lambda)
     }
-
-    ## update latent variables (category/component assignments)
-
-    for (j in 1:nObs) {
-        probs <- rep(0,k+1)
-        for (co in 1:k) {
-            probs[co] <- dnorm(X[j], mean=MU[co], sd=solve(sqrt(S[co]))) *
-                (N[co]-(Z[j]==comps[co]))
-        }
-        ## draw component parameter values from their priors for unrepresented comps
-        mu <- rnorm(n=1,mean=lambda,sd=solve(sqrt(r)))
-        s <- rgamma(n=1,shape=beta, rate=w)
-        probs[k+1] <- dnorm(X[j],
-                            mean=mu,
-                            sd=solve(sqrt(s))) * alpha
-        newComp <- which(rmultinom(n=1, size=1, prob=probs)==1)
-        if (newComp > nComps) {
-            ## new component
-            nComps <- nComps + 1
-            ## find a new label
-            comps <- append(comps, min(setdiff(1:(max(comps)+1), comps)))
-            Znew <- tail(comps,1)
-            ## store initial component precision and mean
-            S <- c(S,s) #array(c(S, solve(sigma)), c(d,d,nComps))
-            MU <- c(MU,mu) #rbind(MU, mu)
-            ## update counts
-            N <- append(N, 1)
-            N[comps==Z[obs]] <- N[comps==Z[obs]] - 1
-        } else {
-            ## old component
-            ## just update counts:
-            Znew <- comps[newComp]
-            N[newComp] <- N[newComp] + 1
-            N[comps==Z[obs]] <- N[comps==Z[obs]] - 1
-        }
-
-        ## Delete a component if its last observation is removed
-        if (N[comps==Z[obs]] == 0) {
-            whichComp <- which(comps==Z[obs])
-            comps <- comps[-whichComp]
-            nComps <- nComps - 1
-            N <- N[-whichComp]
-            MU <- array(data=MU[-whichComp,], dim=c(nComps,d))
-            S <- array(data=S[,,-whichComp], dim=c(d,d,nComps))
-        }
-
-        ## Record the new component assignment for this observation
-        Z[obs] <- Znew
-    }
-
-    ## visualization
-    compHist(X, Z, comps, N, MU, S)
 }
